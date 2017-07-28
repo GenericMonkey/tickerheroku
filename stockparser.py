a6857f8a1c2da0dc27f11ee75dbc288fb0ad58fa
Pranav <pransrin@umich.edu>
2017-07-28 02:06:09 -0400

    Adding robustness for nonexistent symbols

--- stockparser.py
+++ stockparser.py
@@ -3,18 +3,25 @@ from yahoo_finance import Share
 
 
 def parseStock(ticker):
-    stock = Share(ticker)
-    opening = float(stock.get_open())
-    tickerVals = {}
-    tickerVals['name'] = ticker
-    tickerVals['price'] = round(float(getQuotes(ticker)[0]['LastTradePrice']), 2)
-    tickerVals['pricechange'] = round((tickerVals['price'] - opening), 2)
-    tickerVals['percentchange'] = round((tickerVals['pricechange'] / opening), 2)
-    return tickerVals
-
+    try:
+        stock = Share(ticker)
+        opening = float(stock.get_open())
+        tickerVals = {}
+        tickerVals['name'] = ticker
+        tickerVals['price'] = round(float(getQuotes(ticker)[0]['LastTradePrice']), 2)
+        tickerVals['pricechange'] = round((tickerVals['price'] - opening), 2)
+        tickerVals['percentchange'] = round((tickerVals['pricechange'] / opening), 2)
+        return tickerVals
+    except:
+        tickerVals = {}
+        tickerVals['name'] = 'NOTFOUND'
+        tickerVals['price'] = 0
+        tickerVals['pricechange'] = 0
+        tickerVals['percentchange'] = 0
+        return tickerVals
 
 
 
 
 if __name__ == '__main__':
-    parseStock('AAPL')
+    print (parseStock('Bain'))
